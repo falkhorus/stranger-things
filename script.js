f86e891702b07e760f945b9e1893ede6cff6d147
falkhorus <falkhorus@gmail.com>
2026-07-08 15:31:54 -0300

    Adiciona animacao paralax no hero

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 // Usar Gsap para fazer: SCROLL SUAVE => ANIMAÇÕES => ANIMAÇÕES COM ROLAGEM => ANIMAÇÕES NO TEXTO
 
+gsap.registerPlugin(ScrollTrigger,ScrollSmoother,SplitText);
 
+ ScrollSmoother.create({
+     smooth: 1.5,
+     effects: true
+ })
+
+// Obs: Coloquei um atributo chamado data-speed na tag html
+//  picture para poder movimentar as imagens em paralax
+// através do ScrollSmoother que já reconhece esse atributo.
