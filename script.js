24eb9ea7e5d1431288639954420d65483a778e3c
falkhorus <falkhorus@gmail.com>
2026-07-10 20:13:26 -0300

    cria animações para o footer

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ gsap.registerPlugin(ScrollTrigger,ScrollSmoother,SplitText);
 
 // ANIMAÇÕES HERO
 
-gsap.from(".hero", {
+gsap.from(".hero", {   // O From cria uma animação inicial e o TO cria para uma animação que já foi iniciada.
     opacity: 0,
     duration: 1
 })
@@ -37,16 +37,32 @@ gsap.from("picture:nth-child(1)", {
 
 gsap.from(".card", {
     opacity: 0,
-    duration: 1, 
+    // duration: 1, 
     y: 30,
     filter: "blur(20px)",
-    stagger: .3,
+    stagger: .5,
 
     scrollTrigger: {
         trigger: ".cards",
         // markers: true,
-        start: "0% 75%"
+        start: "0% 75%",
+        end: "100% 70%",
+        scrub: true
 
     }
     
+})
+
+// ANIMAÇÕES FOOTER
+
+gsap.from("footer", {  
+    y: "-30%",                  // O Y negativo faz a animação subir e o positivo descer
+    immediateRender: false,     // O immediateRender: false faz com que a animação só seja executada quando o scroll chegar no footer
+
+    scrollTrigger: {            // O scrollTrigger faz com que a animação seja executada quando o scroll chegar no footer
+        trigger: "footer",      
+        scrub: true,            // O scrub: true faz com que a animação seja executada de acordo com o scroll, ou seja, se o scroll estiver no meio da animação, a animação também estará no meio.
+        invalidateOnRefresh: true,  // O invalidateOnRefresh: true faz com que a animação seja reiniciada quando o scroll for atualizado, ou seja, quando o usuário redimensionar a tela.
+        end: "100% 100%"            // O end: "100% 100%" faz com que a animação seja executada até o final do footer, ou seja, quando o scroll chegar no final do footer.
+    }
 })
